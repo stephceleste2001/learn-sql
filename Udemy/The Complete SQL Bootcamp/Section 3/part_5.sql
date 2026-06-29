d677f19ff8290255aff34f395298a950dce958e7
Stephanie Foley <stephceleste2001@gmail.com>
2026-06-29 15:55:28 -0400

    Update README

---
+++ Udemy/The Complete SQL Bootcamp/Section 3/part_5.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69356009011b6abf1daebb30a51d8e8d14b4d91a9aad827d1cb254b690891205
+size 859
---
+++ Udemy/The Complete SQL Bootcamp/Section 3/part_6.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b809c8f167ccdfb5f52d4abf48477036aea4c2e05611cfd5afa41c45d29c01b
+size 652
